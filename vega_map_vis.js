6a3ac4ffe18de590008c13a97a4cab636f8f5134
szha0220 <szha0220@student.monash.edu>
2024-09-22 10:54:07 +1000

    Add files via upload
    Added map contents

---
+++ vega_map_vis.js
@@ -0,0 +1,4 @@
+var vg_2 = "./AustralianAverageMaxTempSpec.json"; // Reference the new spec file
+vegaEmbed("#map_chart", vg_2).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
